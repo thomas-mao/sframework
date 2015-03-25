ddb367771b961600531be6238a285c3fd730e81d
Chamnab <carl.choung@pathmazing.com>
2015-03-25 16:37:17 +0700

    st

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,7 +3,7 @@ require_once('app/views/layout/header.php');
 ?>
 
 <div class="row">
-  <div class="col-sm-4">master</div>
+  <div class="col-sm-4">master-abc</div>
   <div class="col-sm-4">.col-sm-4</div>
   <div class="col-sm-4">.col-sm-4</div>
 </div>
